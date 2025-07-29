9db08fad740e0342d0811a09fb33469b44fc197c
nyhave <nyhave@gmail.com>
2025-07-29 09:44:07 +0300

    Add demo portfolio display

---
+++ docs/demoPortfolio.js
@@ -0,0 +1,20 @@
+window.demoPortfolio = {
+  invested: 1500000,
+  holdings: [
+    { ticker: 'AMZN', weight: 7 },
+    { ticker: 'AXP', weight: 7 },
+    { ticker: 'C', weight: 5.25 },
+    { ticker: 'GE', weight: 6 },
+    { ticker: 'GOOG', weight: 7.7 },
+    { ticker: 'GS', weight: 5 },
+    { ticker: 'IAG.L', weight: 6.4 },
+    { ticker: 'JPM', weight: 5.42 },
+    { ticker: 'META', weight: 5.92 },
+    { ticker: 'MSFT', weight: 7.29 },
+    { ticker: 'NFLX', weight: 9.25 },
+    { ticker: 'NVDA', weight: 3.09 },
+    { ticker: 'PLTR', weight: 5.18 },
+    { ticker: 'RR.L', weight: 6.28 },
+    { ticker: 'SNOW', weight: 6.88 },
+  ]
+};
